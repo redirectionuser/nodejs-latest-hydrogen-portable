dd92a0b5dfa7cb47ce0dd6c413a2b82501f87f06
Redirection User <redirection.user@outlook.com>
2023-07-06 10:10:22 +0200

    changed installation dir to a versionable one

--- Other/Source/unpack.bat
+++ Other/Source/unpack.bat
@@ -1,4 +1,4 @@
 cd App
 7z.exe x node*.zip
-for /f "delims=" %%i in ('dir /b /a:d node*') do move %%i "NodeJsLatest"
+for /f "delims=" %%i in ('dir /b /a:d node*') do move %%i "NodeJsLatestHydrogen"
 del node*.zip
